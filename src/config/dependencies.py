094bd21b03a4a6e7e1b3dc5250aeb526b74f96c9
Marharyta Zazulia <zazmargo@gmail.com>
2025-02-20 02:48:44 -0300

    setting up MailHog(using docker), email-notifications, activation_request

---
+++ src/config/dependencies.py
@@ -0,0 +1,27 @@
+import os
+
+from fastapi import Depends
+
+from src.config.settings import Settings, BaseAppSettings
+from src.notifications import EmailSenderInterface, EmailSender
+
+
+def get_settings() -> BaseAppSettings:
+    return Settings()
+
+
+def get_accounts_email_notificator(
+    settings: BaseAppSettings = Depends(get_settings),
+) -> EmailSenderInterface:
+    return EmailSender(
+        hostname=settings.EMAIL_HOST,
+        port=settings.EMAIL_PORT,
+        email=settings.EMAIL_HOST_USER,
+        password=settings.EMAIL_HOST_PASSWORD,
+        use_tls=settings.EMAIL_USE_TLS,
+        template_dir=settings.PATH_TO_EMAIL_TEMPLATES_DIR,
+        activation_email_template_name=settings.ACTIVATION_EMAIL_TEMPLATE_NAME,
+        activation_complete_email_template_name=settings.ACTIVATION_COMPLETE_EMAIL_TEMPLATE_NAME,
+        password_email_template_name=settings.PASSWORD_RESET_TEMPLATE_NAME,
+        password_complete_email_template_name=settings.PASSWORD_RESET_COMPLETE_TEMPLATE_NAME,
+    )
--- src/config/settings.py
+++ src/config/settings.py
@@ -11,9 +11,12 @@ class BaseAppSettings(BaseSettings):
 
     PATH_TO_EMAIL_TEMPLATES_DIR: str = str(BASE_DIR / "notifications" / "templates")
     ACTIVATION_EMAIL_TEMPLATE_NAME: str = "activation_request.html"
+    ACTIVATION_COMPLETE_EMAIL_TEMPLATE_NAME: str = "activation_complete.html"
+    PASSWORD_RESET_TEMPLATE_NAME: str = "password_reset_request.html"
+    PASSWORD_RESET_COMPLETE_TEMPLATE_NAME: str = "password_reset_complete.html"
 
-    EMAIL_HOST: str = os.getenv("EMAIL_HOST", "host")
-    EMAIL_PORT: int = int(os.getenv("EMAIL_PORT", 25))
+    EMAIL_HOST: str = os.getenv("EMAIL_HOST", "localhost")
+    EMAIL_PORT: int = int(os.getenv("EMAIL_PORT", 1025))
     EMAIL_HOST_USER: str = os.getenv("EMAIL_HOST_USER", "testuser")
     EMAIL_HOST_PASSWORD: str = os.getenv("EMAIL_HOST_PASSWORD", "test_password")
     EMAIL_USE_TLS: bool = os.getenv("EMAIL_USE_TLS", "False").lower() == "true"
