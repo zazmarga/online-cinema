66a803d950840df6a5fc788ab85acb1ebf162126
Marharyta Zazulia <zazmargo@gmail.com>
2025-02-20 16:07:50 -0300

    add schemas for register new user with email & password

---
+++ src/schemas/accounts.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel, EmailStr, field_validator
+
+from src.database.validators.accounts import validate_password_strength
+
+
+class BaseEmailPasswordSchema(BaseModel):
+    email: EmailStr
+    password: str
+
+    model_config = {"from_attributes": True}
+
+    @field_validator("email")
+    @classmethod
+    def validate_email(cls, value):
+        return value.lower()
+
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, value):
+        return validate_password_strength(value)
+
+
+class UserRegistrationRequestSchema(BaseEmailPasswordSchema):
+    pass
+
+
+class UserRegistrationResponseSchema(BaseModel):
+    id: int
+    email: EmailStr
+
+    model_config = {
+        "from_attributes": True
+    }
